e5e412c9b46eb51b3ec2fc80d6893218bb3a123d
zaabed <zaaabedkhaan@gmail.com>
2023-07-22 01:23:19 +0600

    category tab

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,13 @@ import banner1 from '../../../assets/images/banner1.avif';
 import banner2 from '../../../assets/images/banner2.avif';
 import banner3 from '../../../assets/images/banner3.avif';
 import banner4 from '../../../assets/images/banner4.avif';
+import c1 from '../../../assets/images/c1.jpg';
+import c2 from '../../../assets/images/c2.jpg';
+import c3 from '../../../assets/images/c3.jpg';
+import c4 from '../../../assets/images/c4.jpg';
+import c5 from '../../../assets/images/c5.jpg';
+import c6 from '../../../assets/images/c6.jpg';
+import ShopeCategory from '../ShopeCategory/ShopeCategory';
 
 
 const Home = () => {
@@ -34,68 +41,77 @@ const Home = () => {
                 <p className='font-bold text-center'>SHOP <span className='text-red-600 '>TOY TITANS</span></p>
                 <h1 className='mt-3 font-bold text-center text-5xl'>POPULAR IN<span className='text-purple-500'> OUR STORE</span></h1>
                 <div className='grid grid-cols-1 lg:grid-cols-3 justify-items-center'>
-                    <div className="card w-96 bg-base-100 shadow-xl mt-10">
+                    <div className="card w-96 h-96 bg-base-100 shadow-xl mt-10">
                         <figure className="px-10 pt-10">
-                            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            <img src={c4} alt="Shoes" className="rounded-xl" />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <h2 className="card-title">Trucks</h2>
+                            <p>$15</p>
 
                         </div>
                     </div>
-                    <div className="card w-96 bg-base-100 shadow-xl mt-10">
+                    <div className="card w-96 h-96 bg-base-100 shadow-xl mt-10">
                         <figure className="px-10 pt-10">
-                            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            <img src={c2} alt="Shoes" className="rounded-xl" />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <h2 className="card-title">Machine Wheel</h2>
+                            <p>$19</p>
 
                         </div>
                     </div>
-                    <div className="card w-96 bg-base-100 shadow-xl mt-10">
+                    <div className="card w-96 h-96 bg-base-100 shadow-xl mt-10">
                         <figure className="px-10 pt-10">
-                            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            <img src={c3} alt="Shoes" className="rounded-xl" />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <h2 className="card-title">Shovel</h2>
+                            <p>$25</p>
 
                         </div>
                     </div>
-                    <div className="card w-96 bg-base-100 shadow-xl mt-10">
+                    <div className="card w-96 h-96 bg-base-100 shadow-xl mt-10">
                         <figure className="px-10 pt-10">
-                            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            <img src={c1} alt="Shoes" className="rounded-xl" />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <h2 className="card-title">Burnout Rescue</h2>
+                            <p>$15</p>
 
                         </div>
                     </div>
-                    <div className="card w-96 bg-base-100 shadow-xl mt-10">
+                    <div className="card w-96 h-96 bg-base-100 shadow-xl mt-10">
                         <figure className="px-10 pt-10">
-                            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            <img src={c5} alt="Shoes" className="rounded-xl" />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                            <h2 className="card-title">Ruler</h2>
+                            <p>$11</p>
 
                         </div>
                     </div>
-                    <div className="card w-96 bg-base-100 shadow-xl mt-10">
+                    <div className="card w-96 h-96 bg-base-100 shadow-xl mt-10">
                         <figure className="px-10 pt-10">
-                            <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" className="rounded-xl" />
+                            <img src={c6} alt="Shoes" className="rounded-xl " />
                         </figure>
                         <div className="card-body items-center text-center">
-                            <h2 className="card-title">Shoes!</h2>
-                            <p>If a dog chews shoes whose shoes does he choose?</p>
-
+                            <h2 className="card-title">Toluca</h2>
+                            <p>$17</p>
                         </div>
                     </div>
                 </div>
             </div>
+
+            {/* <div className='mt-20'>
+                <h1 className='mt-3 font-bold text-center text-5xl'>OUR <span className='text-purple-500'>SERVICES</span></h1>
+                <div>
+
+                </div>
+            </div> */}
+
+            <ShopeCategory></ShopeCategory>
+
         </div>
     );
 };
